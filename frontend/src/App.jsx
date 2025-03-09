54ce84a6d91fb22bd05409fcb4d415d4527d546a
omarzydan610 <omarzydan610@gmail.com>
2025-03-10 00:06:56 +0200

    initialize repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// ./src/App.js
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl text-white font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add the paths to your JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
